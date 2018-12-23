770f3c906aee08c57a5ef575ab55dcac18a494bc
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-12-23 15:10:34 -0500

    moved image into imagecard component

--- src/Components/ImageList.js
+++ src/Components/ImageList.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import './ImageList.css'
+import ImageCard from './ImageCard'
 
 const ImageList = (props) => {
   let pics = props.images.map((image) => {
-    return <img key={image.id} src={image.urls.small} alt={image.description}/>
+    return <ImageCard key={image.id} image={image}/>
   })
 
   return (
-    <div>
+    <div className="image-list">
       {pics}
     </div>)
 }
